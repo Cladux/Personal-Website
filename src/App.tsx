0b8bcc7d7b7f374c77d72f89343713658d77ee36
Soroush Sharifi <sharifysoroush@gmail.com>
2023-11-04 17:03:17 +0330

    starting header dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.scss";
+import Nav from "./components/nav/Nav";
+import AboutMe from "./layouts/aboutMe/AboutMe";
+import Header from "./layouts/header/Header";
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <AboutMe />
+    </>
+  );
 };
 
 export default App;
---
+++ src/layouts/aboutMe/AboutMe.tsx
@@ -0,0 +1,10 @@
+import styles from "./AboutMe.module.scss";
+const AboutMe = () => {
+  return (
+    <>
+      <section className={styles.sections}></section>
+    </>
+  );
+};
+
+export default AboutMe;

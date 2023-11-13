d391e545a92e3269fe1995fda8545e0c78798e91
Soroush Sharifi <sharifysoroush@gmail.com>
2023-11-13 17:57:44 +0330

    create-project-section get-github-repos-api

--- src/layouts/Projects/Projects.tsx
+++ src/layouts/Projects/Projects.tsx
@@ -1,7 +1,54 @@
+import { useState, useEffect } from "react";
 import styles from "./Projects.module.scss";
+import Icons from "../../icons";
+type Repos = {
+  id: number;
+  name: string;
+  svn_url: string;
+  owner: {
+    avatar_url: string;
+    type: string;
+  };
+};
+// type Icons = keyof typeof Icons;
 
 const Projects = (): JSX.Element => {
-  return <>Projects</>;
+  const [repos, setRepos] = useState<Repos[]>([]);
+  const IconUi = Icons["bxLayout"];
+  const IconRight = Icons["arrowRight"];
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Cladux/repos")
+      .then((res) => res.json())
+      .then((data) => setRepos(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <>
+      <div>
+        {repos.map((repo, id) => (
+          <div className={styles.card} key={id}>
+            <img src={repo.owner.avatar_url} alt={repo.owner.type} />
+            <h4>{repo.name}</h4>
+            <p>
+              <i>
+                <IconUi />
+              </i>
+              Front-end Development
+            </p>
+            <div className={styles.links}>
+              <a href={repo.svn_url}>
+                view code
+                <IconRight />
+              </a>
+              <a href=""></a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Projects;
